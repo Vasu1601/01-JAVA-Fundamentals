bc8e053ca03252e7c390ebac21655084a350eed0
Vassu Mehta <vasum995@gmail,com>
2024-06-22 23:40:29 +0530

    This and Super Method Complete

--- 15_This Method/java1.java
+++ 15_This Method/java1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 
 class A{
@@ -43,50 +42,4 @@ class java1{
        But both can be used only one time inside a constructor.
        - Both are non-static keywords.
 ======================================================================================================================================
-=======
-import java.util.*;
-
-class A{
-    public A(){
-        System.out.println("in A");
-    }
-
-    public A(int n){
-        System.out.println("in A int");
-    }
-}
-
-class B extends A{
-    public B(){
-        System.out.println("in B");
-    }
-
-    public B(int n){
-        this();
-        System.out.println("in B int");
-    }
-}
-
-class java1{
-
-    public static void main(String[] args){
-        B obj = new B(5);
-    }
-
-}
-
-/*
-================================================Notes=================================================================================
-    1. This() method:-
-       - this() method calls the constructor of the same class.
-       - If you do not pass any parameter in this method, then it will call the default constructor of the same class.
-       -  If you pass any parameter in this method, then it will call the parameterised constructor of the same class.
-    
-    2. Similarities in between this() and super() method:-
-       - We can use both this and super method anywhere except the static area.
-       - Both this and super method can be used any number of times in a code.
-       But both can be used only one time inside a constructor.
-       - Both are non-static keywords.
-======================================================================================================================================
->>>>>>> 73771807fe1c45f37b392d39481ed757d8c99088
 */
