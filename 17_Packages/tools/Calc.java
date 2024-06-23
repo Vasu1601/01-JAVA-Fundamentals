04992e48510cf5bd86e41c2349744ba51e2dee77
Vassu Mehta <vasum995@gmail,com>
2024-06-23 19:52:55 +0530

    Packages In java Completed

--- 17_Packages/tools/Calc.java
+++ 17_Packages/tools/Calc.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tools;
 
 public class Calc{
@@ -6,13 +5,4 @@ public class Calc{
     public void show(){
         System.out.println("Hello This is Calc");
     }
-=======
-package tools;
-
-public class Calc{
-
-    public void show(){
-        System.out.println("Hello This is Calc");
-    }
->>>>>>> c800b1c95f1dfbe1d49a43698ed12bd07c20369f
 }
