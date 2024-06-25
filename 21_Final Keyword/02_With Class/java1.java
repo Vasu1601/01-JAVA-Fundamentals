47f89bc8345133f2019e165952d5ac0e1cbb1273
Vassu Mehta <vasum995@gmail,com>
2024-06-25 14:51:39 +0530

    Final Keyword Complete

--- 21_Final Keyword/02_With Class/java1.java
+++ 21_Final Keyword/02_With Class/java1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 final class Calc{
     public void show(){
         System.out.println("This is Calc");
@@ -26,33 +25,4 @@ class java1{
     The final keyword in case of class is basically used to stop the inheritance and if you make any class final and then try to 
     extends the class then it will give an error.
 ======================================================================================================================================
-=======
-final class Calc{
-    public void show(){
-        System.out.println("This is Calc");
-    }
-
-    public void add(int a, int b){
-        System.out.println(a + b);
-    }
-}
-
-class AdvCalc extends Calc{
-
-}
-
-class java1{
-    public static void main(String[] args){
-
-        AdvCalc obj = new AdvCalc();
-        obj.add(5,6);
-    }
-}
-
-/*
-================================================Notes=================================================================================
-    The final keyword in case of class is basically used to stop the inheritance and if you make any class final and then try to 
-    extends the class then it will give an error.
-======================================================================================================================================
->>>>>>> 9811f54bcf046af045a6ff64ab2c2afaf7db6fd1
 */
